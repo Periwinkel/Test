7479476835ef07cf70899e02acf607b7219120b0
Periwinkel <41834302+Periwinkel@users.noreply.github.com>
2018-07-28 19:21:40 +0200

    Experimentiert mit den Möglichkeiten zur UI Gestaltung

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -32,21 +32,35 @@
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.Sidedraw = new System.Windows.Forms.Panel();
+            this.Rechner = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Sidedraw.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 203);
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 45);
+            this.button1.Size = new System.Drawing.Size(196, 70);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Summe";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "SUMME";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(257, 94);
+            this.maskedTextBox1.Location = new System.Drawing.Point(257, 116);
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.Size = new System.Drawing.Size(163, 26);
             this.maskedTextBox1.TabIndex = 1;
@@ -68,17 +82,123 @@
             this.maskedTextBox3.TabIndex = 3;
             this.maskedTextBox3.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox3_MaskInputRejected);
             // 
+            // Sidedraw
+            // 
+            this.Sidedraw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Sidedraw.Controls.Add(this.button4);
+            this.Sidedraw.Controls.Add(this.button3);
+            this.Sidedraw.Controls.Add(this.button2);
+            this.Sidedraw.Controls.Add(this.Rechner);
+            this.Sidedraw.Controls.Add(this.button1);
+            this.Sidedraw.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Sidedraw.Location = new System.Drawing.Point(0, 0);
+            this.Sidedraw.Name = "Sidedraw";
+            this.Sidedraw.Size = new System.Drawing.Size(196, 450);
+            this.Sidedraw.TabIndex = 6;
+            // 
+            // Rechner
+            // 
+            this.Rechner.AutoSize = true;
+            this.Rechner.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Rechner.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rechner.ForeColor = System.Drawing.Color.White;
+            this.Rechner.Location = new System.Drawing.Point(0, 0);
+            this.Rechner.Name = "Rechner";
+            this.Rechner.Size = new System.Drawing.Size(180, 37);
+            this.Rechner.TabIndex = 7;
+            this.Rechner.Text = "RECHNER";
+            this.Rechner.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SandyBrown;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 70);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "SUBTRAKTION";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SandyBrown;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 70);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "PRODUKT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SandyBrown;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(0, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(196, 70);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "DIVISION";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(196, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(604, 37);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(569, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 37);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "X";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Sidedraw);
             this.Controls.Add(this.maskedTextBox3);
             this.Controls.Add(this.maskedTextBox2);
             this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Sidedraw.ResumeLayout(false);
+            this.Sidedraw.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +210,13 @@
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
         private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Panel Sidedraw;
+        private System.Windows.Forms.Label Rechner;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,9 +19,9 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             float Summe = float.Parse(maskedTextBox1.Text) + float.Parse(maskedTextBox2.Text);
             maskedTextBox3.Text = Summe.ToString();
-
 
 
         }
@@ -39,6 +39,76 @@ namespace WindowsFormsApp1
         private void maskedTextBox3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
 
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float Summe = float.Parse(maskedTextBox1.Text) - float.Parse(maskedTextBox2.Text);
+            maskedTextBox3.Text = Summe.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float Summe = float.Parse(maskedTextBox1.Text) * float.Parse(maskedTextBox2.Text);
+            maskedTextBox3.Text = Summe.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float Summe = float.Parse(maskedTextBox1.Text) / float.Parse(maskedTextBox2.Text);
+            maskedTextBox3.Text = Summe.ToString();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private bool _Moving = false;
+        private Point _Offset;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _Moving = true;
+            _Offset = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_Moving)
+            {
+                Point newlocation = this.Location;
+                newlocation.X += e.X - _Offset.X;
+                newlocation.Y += e.Y - _Offset.Y;
+                this.Location = newlocation;
+            }
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_Moving)
+            {
+                _Moving = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
         }
     }
 }
